7950ff636b19c6d1d7f591029e09aa466ad4f6a9
jad113818 <jad113818@gmail.com>
2025-04-30 13:49:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
         # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        
         sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
    
     # st.write(ingredients_string)
@@ -59,6 +59,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
     import requests
-
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.text(smoothiefroot_response)
 
 
