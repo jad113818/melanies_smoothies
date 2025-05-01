5b36be1bf26c0a24aae5c7e782bd904549d82287
jad113818 <jad113818@gmail.com>
2025-05-01 09:25:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,14 +43,14 @@ if ingredients_list:
                 smoothiefroot_response = requests.get("https://my.smoothiefroot/api/fruit/" + fruit_chosen)   
                 sd_df = st.dataframe (data= smoothiefroot_response.json(), use_container_width=True)
 
-    # st.write(ingredients_string)
+      # st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+      my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string+ """', '""" +name_on_order+"""')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
-    time_to_insert = st.button ('Submit Order')
+      #st.write(my_insert_stmt)
+      #st.stop()
+      time_to_insert = st.button ('Submit Order')
     
     if  time_to_insert:
         session.sql(my_insert_stmt).collect()    
